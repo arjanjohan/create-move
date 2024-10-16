a484dc4b8e25a9405660cd1168520ffbca8bb364
arjanjohan <arjanvanhoogdalem@gmail.com>
2024-10-16 08:19:55 +0200

    init

---
+++ src/cli.ts
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+console.log('🚀 Move project setup is coming soon! Stay tuned.');
+console.log('🔗 Check out the project repository at: https://github.com/arjanjohan/scaffold-move');
